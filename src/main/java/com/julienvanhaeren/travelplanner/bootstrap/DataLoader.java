a4447b53fed6c38f40fccd3ec115893682e83583
jvhaeren <julienvanhaeren@gmail.com>
2019-10-25 13:57:12 +0200

    Added more bootstrap data and created a view with Thymeleaf

--- src/main/java/com/julienvanhaeren/travelplanner/bootstrap/DataLoader.java
+++ src/main/java/com/julienvanhaeren/travelplanner/bootstrap/DataLoader.java
@@ -1,5 +1,6 @@
 package com.julienvanhaeren.travelplanner.bootstrap;
 
+import com.julienvanhaeren.travelplanner.model.Accommodation;
 import com.julienvanhaeren.travelplanner.model.Destination;
 import com.julienvanhaeren.travelplanner.model.Meal;
 import com.julienvanhaeren.travelplanner.model.Trip;
@@ -38,18 +39,62 @@ public class DataLoader implements CommandLineRunner {
 
     private void loadData(){
         Trip trip1 = new Trip();
-        trip1.setName("india");
+        trip1.setName("India");
 
         Destination jaipur = new Destination();
         jaipur.setCity("Jaipur");
         jaipur.setTrip(trip1);
 
-        Meal meal1 = new Meal();
-        meal1.setName("maaltijd");
-        meal1.setDestination(jaipur);
+        Destination udaipur = new Destination();
+        udaipur.setCity("Udaipur");
+        udaipur.setTrip(trip1);
 
-        jaipur.getMeals().add(meal1);
+        Meal meal11 = new Meal();
+        meal11.setName("ontbijt jaipur");
+        meal11.setDestination(jaipur);
+
+        Meal meal12 = new Meal();
+        meal12.setName("lunch jaipur");
+        meal12.setDestination(jaipur);
+
+        Meal meal13 = new Meal();
+        meal13.setName("diner jaipur");
+        meal13.setDestination(jaipur);
+
+        Accommodation hotel1 = new Accommodation();
+        hotel1.setName("Palace Hotel");
+        hotel1.setDestination(jaipur);
+        Accommodation hotel2 = new Accommodation();
+        hotel2.setName("Hi Hostel");
+        hotel2.setDestination(udaipur);
+
+        Meal meal21 = new Meal();
+        meal21.setName("ontbijt udaipur");
+        meal21.setDestination(udaipur);
+
+        Meal meal22 = new Meal();
+        meal22.setName("lunch udaipur");
+        meal22.setDestination(udaipur);
+
+        Meal meal23 = new Meal();
+        meal23.setName("diner udaipur");
+        meal23.setDestination(udaipur);
+
+
+        jaipur.getMeals().add(meal11);
+        jaipur.getMeals().add(meal12);
+        jaipur.getMeals().add(meal13);
+        jaipur.getAccommodations().add(hotel1);
         trip1.getDestinations().add(jaipur);
+
+        udaipur.getMeals().add(meal21);
+        udaipur.getMeals().add(meal22);
+        udaipur.getMeals().add(meal23);
+        udaipur.getAccommodations().add(hotel2);
+        trip1.getDestinations().add(udaipur);
+
+        tripService.save(trip1);
+
     }
 
 }
--- src/main/java/com/julienvanhaeren/travelplanner/controllers/IndexController.java
+++ src/main/java/com/julienvanhaeren/travelplanner/controllers/IndexController.java
@@ -1,12 +1,28 @@
 package com.julienvanhaeren.travelplanner.controllers;
 
+import com.julienvanhaeren.travelplanner.services.DestinationService;
+import com.julienvanhaeren.travelplanner.services.MealService;
+import com.julienvanhaeren.travelplanner.services.TripService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
+
+    private final TripService tripService;
+    private final DestinationService destinationService;
+    private final MealService mealService;
+
+    public IndexController(TripService tripService, DestinationService destinationService, MealService mealService) {
+        this.tripService = tripService;
+        this.destinationService = destinationService;
+        this.mealService = mealService;
+    }
+
     @RequestMapping({"", "/", "index", "index.html"})
-    public String index(){
+    public String index(Model model){
+        model.addAttribute("trips", tripService.findAll());
         return "index";
     }
 }
